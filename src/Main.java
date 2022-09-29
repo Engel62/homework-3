d8cf91311f58c78bb0792c37bb906f9ac3811cbf
Engel62 <sr.engel@bk.ru>
2022-09-29 16:48:18 +0300

    разница веса бойцов

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        var firstBoxer = 78.2;
+        var secondBoxer = 82.7;
+        var totalWeight = firstBoxer + secondBoxer;
+        System.out.println("Общий вес двух бойцов " + totalWeight + "кг!");
+
+        var weightDifferent = (firstBoxer - secondBoxer) % totalWeight;
+        System.out.println("Разница веса бойцов" + weightDifferent + "кг");
+
     }
 }
